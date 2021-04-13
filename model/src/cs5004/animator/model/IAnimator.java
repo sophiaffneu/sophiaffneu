2f32a3e8cde9c88b6806f20599407a79bd89302b
Jiayi0928 <mmajiayi@163.com>
2021-04-13 13:52:35 -0400

    add 4 getters

--- model/src/cs5004/animator/model/IAnimator.java
+++ model/src/cs5004/animator/model/IAnimator.java
@@ -18,6 +18,11 @@ public interface IAnimator {
    */
   IAnimator setBounds(int x, int y, int width, int height);
 
+  int getX();
+  int getY();
+  int getWidth();
+  int getHeight();
+
   /**
    * Add a shape.
    *
