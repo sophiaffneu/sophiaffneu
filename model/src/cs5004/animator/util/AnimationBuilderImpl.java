edb64b1964fde9d2e504cc3be31945edb5deba66
sophiaffneu <77813893+sophiaffneu@users.noreply.github.com>
2021-04-13 02:13:54 -0700

    correct some logic error

--- model/src/cs5004/animator/util/AnimationBuilderImpl.java
+++ model/src/cs5004/animator/util/AnimationBuilderImpl.java
@@ -44,6 +44,9 @@ public class AnimationBuilderImpl implements AnimationBuilder {
       nShape = new Oval(name);
     }
     m.addShape(nShape);
+    //for (IShape n : m.getShapeList()) {
+    //System.out.println(n.getName().toString());
+    //}
     return this;
   }
 
@@ -53,65 +56,72 @@ public class AnimationBuilderImpl implements AnimationBuilder {
 
       if (n.getName().equals(name)) {
 
-        if (n.getPosition() == null) {
+        if (n.getPosition() == null && n.getShapeProperty() == null && n.getColor() == null) {
           n.setPosition(new Point2D(x1, y1));
           n.setShapeProperty(new ShapeProperty(w1, h1));
           n.setColor(new Color(r1, g1, b1));
-        } else {
-          if (m.getTransList().size() == 0 || (m.getTransList().size() > 0 &&
-                  m.getTransList().stream().filter(t -> t.getTransShape().getName().equals(n.getName())).count() == 0)) {
-            IShape copyS = n.copyShape();
 
-            if (x1 != x2 || y1 != y2) {
 
-              ITransformation newMove = new Move(copyS, new TimePeriod(t1, t2), new Point2D(x2, y2));
+          //if (m.getTransList().size() == 0 || (m.getTransList().size() > 0 &&
+          // m.getTransList().stream().filter(t -> t.getTransShape().getName().equals(n.getName())).count() == 0)) {
+          IShape copyS = n.copyShape();
 
-              m.addTransformations(newMove);
-            }
-            if (w1 != w2 || h1 != h2) {
-              ITransformation newScale = new Scale(copyS, new TimePeriod(t1, t2), new ShapeProperty(w2, h2));
+          if (x1 != x2 || y1 != y2) {
 
-              m.addTransformations(newScale);
-            }
-            if (r1 != r2 || g1 != g2 || b1 != b2) {
+            ITransformation newMove = new Move(copyS, new TimePeriod(t1, t2), new Point2D(x2, y2));
 
-              ITransformation newColorChange = new ChangeColor(copyS, new TimePeriod(t1, t2), new Color(r2, g2, b2));
-
-              m.addTransformations(newColorChange);
-            } else {
-              ITransformation newNoChange = new NoChange(copyS, new TimePeriod(t1, t2));
-            }
-
-          } else {
-
-            List<ITransformation> transList = m.getTransList().stream().filter(t -> t.getTransShape().getName().equals(n.getName())).collect(Collectors.toList());
-            IShape transShape = transList.get(0).getTransShape();
-
-            if (x1 != x2 || y1 != y2) {
-
-              ITransformation newMove = new Move(transShape, new TimePeriod(t1, t2), new Point2D(x2, y2));
-
-              m.addTransformations(newMove);
-            }
-            if (w1 != w2 || h1 != h2) {
-              ITransformation newScale = new Scale(transShape, new TimePeriod(t1, t2), new ShapeProperty(w2, h2));
-              System.out.println(newScale.toString());
-              m.addTransformations(newScale);
-            }
-            if (r1 != r2 || g1 != g2 || b1 != b2) {
-
-              ITransformation newColorChange = new ChangeColor(transShape, new TimePeriod(t1, t2), new Color(r2, g2, b2));
-
-              m.addTransformations(newColorChange);
-            } else {
-              ITransformation newNoChange = new NoChange(transShape, new TimePeriod(t1, t2));
-            }
-
+            m.addTransformations(newMove);
           }
+          if (w1 != w2 || h1 != h2) {
+            ITransformation newScale = new Scale(copyS, new TimePeriod(t1, t2), new ShapeProperty(w2, h2));
+
+            m.addTransformations(newScale);
+          }
+          if (r1 != r2 || g1 != g2 || b1 != b2) {
+
+            ITransformation newColorChange = new ChangeColor(copyS, new TimePeriod(t1, t2), new Color(r2, g2, b2));
+
+            m.addTransformations(newColorChange);
+          }
+          if (x1 == x2 && y1 == y2 && w1 == w2 && h1 == h2 && r1 == r2 && g1 == g2 && b1 == b2) {
+            ITransformation newNoChange = new NoChange(copyS, new TimePeriod(t1, t2));
+            m.addTransformations(newNoChange);
+          }
+
+        } else {
+
+          List<ITransformation> transList = m.getTransList().stream().filter(t -> t.getTransShape().getName().equals(n.getName())).collect(Collectors.toList());
+          IShape transShape = transList.get(0).getTransShape();
+
+          if (x1 != x2 || y1 != y2) {
+
+            ITransformation newMove = new Move(transShape, new TimePeriod(t1, t2), new Point2D(x2, y2));
+
+            m.addTransformations(newMove);
+          }
+          if (w1 != w2 || h1 != h2) {
+            ITransformation newScale = new Scale(transShape, new TimePeriod(t1, t2), new ShapeProperty(w2, h2));
+            System.out.println(newScale.toString());
+            m.addTransformations(newScale);
+          }
+          if (r1 != r2 || g1 != g2 || b1 != b2) {
+
+            ITransformation newColorChange = new ChangeColor(transShape, new TimePeriod(t1, t2), new Color(r2, g2, b2));
+
+            m.addTransformations(newColorChange);
+          }
+          if (x1 == x2 && y1 == y2 && w1 == w2 && h1 == h2 && r1 == r2 && g1 == g2 && b1 == b2) {
+            ITransformation newNoChange = new NoChange(transShape, new TimePeriod(t1, t2));
+            m.addTransformations(newNoChange);
+          }
+
+
         }
-        break;
+break;
       }
+
     }
+
     return this;
   }
 }
