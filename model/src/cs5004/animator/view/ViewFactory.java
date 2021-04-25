c3736dbe3c58a5585a9479a05edfdc25cfb462d6
sophiaffneu <77813893+sophiaffneu@users.noreply.github.com>
2021-04-25 13:33:36 -0700

    Add files via upload

--- model/src/cs5004/animator/view/ViewFactory.java
+++ model/src/cs5004/animator/view/ViewFactory.java
@@ -1,6 +1,13 @@
 package cs5004.animator.view;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import cs5004.animator.model.IAnimator;
+import cs5004.animator.model.IShape;
+import cs5004.animator.model.ITransformation;
 
 /**
  * This class represents a view factory. We can generate different views by it.
@@ -15,17 +22,19 @@ public class ViewFactory {
    * @return a view
    */
   public IView generateView(String viewType, IAnimator model, int tempo) {
+    List<IShape> copyShapeList = new ArrayList<>();
+    List<ITransformation> copyTransList = new ArrayList<>();
+    Collections.copy(copyShapeList, model.getShapeList());
+    Collections.copy(copyTransList, model.getTransList());
     switch (viewType) {
       case "text":
-        return new TextView(model.getShapeList(), model.getTransList());
+        return new TextView(copyShapeList, copyTransList);
       case "svg":
-        return new SVGView(model.getShapeList(), model.getTransList(), tempo, model.getWidth(),
+        return new SVGView(copyShapeList,copyTransList, tempo, model.getWidth(),
             model.getHeight(), model.getX(), model.getY());
       case "visual":
         return new VisualView(model.getX(), model.getY(), model.getWidth(), model.getHeight(),
             tempo);
-      case "playback":
-        return new NewView(model.getX(),model.getY(),model.getWidth(),model.getHeight(),tempo);
       default:
         return null;
     }
